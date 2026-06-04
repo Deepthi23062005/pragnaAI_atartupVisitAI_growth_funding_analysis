51d80e0ab9bf6f8ed8766bc8d445e0fce173591f
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 10:57:54 +0530

    Create ui_styling.py

---
+++ ui_styling.py
@@ -0,0 +1,16 @@
+st.markdown("""
+<style>
+
+.main{
+    background-color:#f7f9fc;
+}
+
+.stMetric{
+    border-radius:15px;
+    padding:15px;
+    background:white;
+    box-shadow:0px 2px 10px rgba(0,0,0,0.1);
+}
+
+</style>
+""", unsafe_allow_html=True)
