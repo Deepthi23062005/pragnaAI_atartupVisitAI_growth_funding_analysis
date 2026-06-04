5cc35c57c5c86e18e3d2a9e48c5f95182affa888
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 10:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Startup Success Analytics",
+    page_icon="🚀",
+    layout="wide"
+)
+
+st.title("🚀 Startup Success Analytics Platform")
+
+st.markdown("""
+Analyze startup ecosystem trends, funding patterns,
+founder impact and predict startup success.
+""")
+
+st.image(
+    "https://images.unsplash.com/photo-1552664730-d307ca884978",
+    use_container_width=True
+)
