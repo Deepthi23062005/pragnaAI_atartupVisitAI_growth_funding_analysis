5e63437f97db299ab3736dd2d1b85d99ad6b8944
Deepthi23062005 <deepthid433@gmail.com>
2026-06-04 10:43:26 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,1 +1,35 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/startup_success_dataset.csv")
+
+c1,c2,c3,c4 = st.columns(4)
+
+c1.metric(
+    "Total Startups",
+    len(df)
+)
+
+c2.metric(
+    "IPO %",
+    round(
+        (df["outcome"]=="IPO").mean()*100,
+        2
+    )
+)
+
+c3.metric(
+    "Acquisition %",
+    round(
+        (df["outcome"]=="Acquisition").mean()*100,
+        2
+    )
+)
+
+c4.metric(
+    "Failure %",
+    round(
+        (df["outcome"]=="Failure").mean()*100,
+        2
+    )
+)
